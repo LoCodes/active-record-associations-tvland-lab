4881edc3d285eae4a38ee8dbea1528ebb74bad84
Loren Cipriano <loren25cipriano@gmail.com>
2021-01-28 19:47:39 -0800

    commit tree f1458fd0f59c5b4a1d67c5f11a582b8a786f320a

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    #can list its full name
+    def full_name 
+        "#{self.first_name} #{self.last_name}"
+    end 
+
+    # can list all of its shows and characters
+    def list_roles
+        self.characters.map do |character|
+            "#{character.name} - #{character.show.name}"
+        end 
+    end 
 end
